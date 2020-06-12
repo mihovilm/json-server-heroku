dc36395e67648bddda38a51957b0ee5640c827c4
Mihovil Mandic <mihovil.mandic.21@dartmouth.edu>
2020-06-12 14:33:02 +0200

    fixed json

---
+++ Untitled.py
@@ -0,0 +1,24 @@
+import json
+
+f = open('olddb.json',)
+
+data = json.load(f)
+
+new = {"internet":[], "velocity":[], "searchVirality":[], "demographic":[], "emerging":[]}
+
+for obj in data:
+    if (obj["type"] == "Demographic"):
+        new["demographic"].append(obj)
+    elif (obj["type"] == "Velocity"):
+        new["velocity"].append(obj)
+    elif (obj["type"] == "Search Virality"):
+        new["searchVirality"].append(obj)
+    elif (obj["type"] == "Internet/TikTok"):
+        new["internet"].append(obj)
+    elif (obj["type"] == "Emerging Artists"):
+        new["emerging"].append(obj)
+
+print(json.dumps(new))
+
+with open('db.json', 'w', encoding='utf-8') as f:
+    json.dump(new, f, indent=4)
